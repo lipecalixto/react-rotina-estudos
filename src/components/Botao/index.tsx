77a63717e1c05c4f03e538c40b2d66d8e1602d73
lipecalixto <fellipe.calixto.santos@gmail.com>
2023-04-17 20:20:57 -0300

    feat: estilizando com css module

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import './style.scss';
+import React from 'react';
+import style from './Botao.module.scss';
 
 class Botao extends React.Component {
-    render() {
-        return (
-            <button className="botao">
-                Botão
-            </button>
-        )
-    }
+  render() {
+    return (
+      <button className={style.botao}>
+        Botão
+      </button>
+    )
+  }
 }
 
 export default Botao;
