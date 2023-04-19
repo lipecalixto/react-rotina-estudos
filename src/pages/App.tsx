4a8336a570b5f7556d242b1e63d7fb334dd09c4b
lipecalixto <fellipe.calixto.santos@gmail.com>
2023-04-19 19:33:12 -0300

    feat: add relogio e cronometro

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import style from './style.module.scss';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
+import Cronometro from '../components/Cronometro';
 
 function App() {
   return (
     <div className={style.AppStyle}>
       <Formulario />
       <Lista />
+      <Cronometro />
     </div>
   );
 }
